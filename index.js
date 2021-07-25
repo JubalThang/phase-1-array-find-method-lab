b8aabf1420b34ea7ec04b107420038a64961af8e
jubalthang <jubalthang@gamil.com>
2021-07-25 14:18:02 -0700

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+function superbowlWin(records) {
+    let ans = null
+    records.find(record => ans = findRecord(record) )
+    return ans != undefined ? ans.year : ans 
+}
+
+function findRecord(record) {
+  
+       return record.result === 'W' ? record : undefined
+}
